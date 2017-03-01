24e552b852a74f1bc9632e2e2cd2e00fcc21d6c8
MorzyReno <934470937@qq.com>
2017-03-01 21:44:23 +0800

    annotation driver

---
+++ src/main/java/bean/Greeting.java
@@ -0,0 +1,25 @@
+package bean;
+
+public class Greeting {
+
+	private String aa;
+	
+	
+	public Greeting(){
+		
+	}
+	
+	public Greeting(String aa){
+		this.aa=aa;
+	};
+
+	public String getAa() {
+		return aa;
+	}
+
+	public void setAa(String aa) {
+		this.aa = aa;
+	}
+	
+	
+}
---
+++ src/main/java/controller/SampleController.java
@@ -0,0 +1,17 @@
+package controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import bean.Greeting;
+
+@RestController
+@RequestMapping("/sample")
+public class SampleController {
+	
+	@RequestMapping("/greeting")
+	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return new Greeting(name);
+	}
+}
